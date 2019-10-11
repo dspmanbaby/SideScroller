11cc46091fad29bfca5a5f267596717bf9abe90f
dspmanbaby <32938970+dspmanbaby@users.noreply.github.com>
2019-10-11 13:57:12 -0700

    Initial commit

---
+++ app/src/main/java/com/example/sidescroller/MechKeyboardDB.java
@@ -0,0 +1,19 @@
+package com.example.sidescroller;
+
+public class MechKeyboardDB {
+
+        public static String[] description = {
+                "",//plating
+                "",//pcb
+                "",//key caps
+                "",//stabalizers
+                "",//switches
+                ""
+                //description of keyboard parts
+        };
+        public static int [] id = {
+
+                //drawables
+
+        };
+}
